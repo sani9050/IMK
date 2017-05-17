467ac910e427c127ba13cedc15dd60f2bca958f3
nezarmahardika <nezarmahardika1@gmail.com>
2017-05-17 19:24:39 +0700

    Converts a non-null value into Visibility.

---
+++ Converters/VisibleIfPresentConverter.cs
@@ -0,0 +1,33 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+
+
+namespace Microsoft.Kinect.Samples.KinectPaint
+{
+    /// <summary>
+    /// Converts any non-null value into Visibility.Visible, and any null value into Visibility.Collapsed
+    /// </summary>
+    public class VisibleIfPresentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null) return Visibility.Collapsed;
+            else return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
