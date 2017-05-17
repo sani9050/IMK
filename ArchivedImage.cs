87a9875201fe345ca27c175eeb7cb17a3889ee46
nezarmahardika <nezarmahardika1@gmail.com>
2017-05-17 21:12:20 +0700

    save image and draw

---
+++ ArchivedImage.cs
@@ -0,0 +1,53 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Globalization;
+
+
+
+namespace Microsoft.Kinect.Samples.KinectPaint
+{
+    /// <summary>
+    /// Represents an image in My Pictures\KinectPaint that can be loaded
+    /// </summary>
+    public class ArchivedImage
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="path">The file path of the image</param>
+        public ArchivedImage(string path)
+        {
+            CreateTime = File.GetLastWriteTime(path);
+            Image = new Uri(path, UriKind.RelativeOrAbsolute);
+            CreateTimeString = CreateTime.ToString("M/d/yyyy", CultureInfo.CurrentCulture) + "\n" + CreateTime.ToString("h:mm tt", CultureInfo.CurrentCulture);
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// The path of the image file
+        /// </summary>
+        public Uri Image { get; private set; }
+
+        /// <summary>
+        /// The time the file was last modified
+        /// </summary>
+        public DateTime CreateTime { get; private set; }
+
+        /// <summary>
+        /// A string representation of the time the file was last modified
+        /// </summary>
+        public string CreateTimeString { get; private set; }
+
+        #endregion
+
+    }
+}
---
+++ BrushSelection.cs
@@ -0,0 +1,70 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace Microsoft.Kinect.Samples.KinectPaint
+{
+    /// <summary>
+    /// Represents the different kinds of 'brushes' available to the user
+    /// </summary>
+    public enum KinectPaintbrush
+    {
+        Brush,
+        Marker,
+        Airbrush,
+        Eraser
+    }
+
+    /// <summary>
+    /// Contains information about a particular kind of brush
+    /// </summary>
+    public class BrushSelection
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="icon">URI of the icon representing the brush</param>
+        /// <param name="iconSelected">URI of the icon representing the brush when it is selected</param>
+        /// <param name="brush">The type of brush</param>
+        /// <param name="friendlyName">The user-friendly name of the brush</param>
+        public BrushSelection(Uri icon, Uri iconSelected, KinectPaintbrush brush, string friendlyName)
+        {
+            Icon = icon;
+            IconSelected = iconSelected;
+            Brush = brush;
+            FriendlyName = friendlyName;
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// URI of the icon representing the brush
+        /// </summary>
+        public Uri Icon { get; private set; }
+
+        /// <summary>
+        /// URI of the icon representing the brush when the tool is selected
+        /// </summary>
+        public Uri IconSelected { get; private set; }
+
+        /// <summary>
+        /// The type of brush
+        /// </summary>
+        public KinectPaintbrush Brush { get; private set; }
+
+        /// <summary>
+        /// The user-friendly name of the brush
+        /// </summary>
+        public string FriendlyName { get; private set; }
+
+        #endregion
+    }
+}
