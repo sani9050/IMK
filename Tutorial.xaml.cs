6cf57996e4ae6392024798dec21114133d397f8b
Brilian Widya Mustofa <brilianwm@gmail.com>
2017-05-18 01:32:43 +0700

    Tutorial

---
+++ Tutorial.xaml.cs
@@ -0,0 +1,26 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved. 
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Microsoft.Kinect.Samples.KinectPaint
+{
+    /// <summary>
+    /// Interaction logic for Tutorial.xaml
+    /// </summary>
+    public partial class Tutorial : UserControl
+    {
+        public Tutorial()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Visibility = Visibility.Collapsed;
+        }
+    }
+}
